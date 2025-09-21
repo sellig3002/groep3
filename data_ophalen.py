f7b6437d3fe9091d475c0b6976caadda62ad5b00
sellig3002 <112904390+sellig3002@users.noreply.github.com>
2025-09-21 13:56:35 +0200

    Update data_ophalen.py

--- data_ophalen.py
+++ data_ophalen.py
@@ -3,17 +3,18 @@ from pathlib import Path
 import os
 import streamlit as st
 
-os.environ["KAGGLE_USERNAME"] = st.secrets["kaggle"]["username"]
-os.environ["KAGGLE_KEY"] = st.secrets["kaggle"]["key"]
-
-api=KaggleApi()
-api.authenticate()
-
-data_dir = Path('data')
-data_dir.mkdir(exist_ok=True)
-
-datasets=['lainguyn123/student-performance-factors','haseebindata/student-performance-predictions']
-
-for dataset in datasets:
-    api.dataset_download_files(dataset, path=data_dir, unzip=True)
+def download_datasets()
+    os.environ["KAGGLE_USERNAME"] = st.secrets["kaggle"]["username"]
+    os.environ["KAGGLE_KEY"] = st.secrets["kaggle"]["key"]
+    
+    api=KaggleApi()
+    api.authenticate()
+    
+    data_dir = Path('data')
+    data_dir.mkdir(exist_ok=True)
+    
+    datasets=['lainguyn123/student-performance-factors','haseebindata/student-performance-predictions']
+    
+    for dataset in datasets:
+        api.dataset_download_files(dataset, path=data_dir, unzip=True)
     
