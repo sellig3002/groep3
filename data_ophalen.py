1ccdbcbd6d1d28e41f387f5a58099a5f270be07d
sellig3002 <degoeijgilles@gmail.com>
2025-09-21 13:16:26 +0200

    kaggle api toevoegen secrets

--- data_ophalen.py
+++ data_ophalen.py
@@ -1,5 +1,10 @@
 from kaggle.api.kaggle_api_extended import KaggleApi
 from pathlib import Path
+import os
+import streamlit as st
+
+os.environ["KAGGLE_USERNAME"] = st.secrets["kaggle"]["username"]
+os.environ["KAGGLE_KEY"] = st.secrets["kaggle"]["key"]
 
 api=KaggleApi()
 api.authenticate()
